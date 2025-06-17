26ff2aa837e01ffa8c0bc1aca695735f4efeaa1d
mohamed <mmm.medox@gmail.com>
2025-06-17 22:53:35 +0300

    connect firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pickit/core/di/dependency_injection.dart';
 import 'package:pickit/core/routing/app_router.dart';
+import 'package:pickit/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
